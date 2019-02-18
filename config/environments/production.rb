b77c159e402e7248bf25d33dadbc4c63d2a66011
Jose <jose@agendapro.com>
2019-02-17 22:57:02 -0300

    production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://morning-ravine-91084.herokuapp.com/', 'http://morning-ravine-91084.herokuapp.com/']
 end
